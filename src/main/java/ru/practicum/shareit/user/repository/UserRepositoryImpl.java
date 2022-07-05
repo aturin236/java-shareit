81ea0c0a5c73fb9c91f9067834e372ae4f656a9f
aturin <aturin@avancore.ru>
2022-07-05 11:16:18 +0300

    fix: code style

--- src/main/java/ru/practicum/shareit/user/repository/UserRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepositoryImpl.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 public class UserRepositoryImpl implements UserRepository {
     private static long idCounter;
     private final List<User> users = new ArrayList<>();
+
     @Override
     public List<User> getAllUsers() {
         return users;
