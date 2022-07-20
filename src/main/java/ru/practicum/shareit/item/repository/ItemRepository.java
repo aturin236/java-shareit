15aa1108ea3a5c2f3e665b9a7a5b4a4d2a8bee68
aturin <aturin@avancore.ru>
2022-07-20 13:49:15 +0300

    fix: отладка тестов4

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByOwner(User owner);
+
     @Query("select i from Item i " +
             "where i.available = true and (upper(i.name) like upper(concat('%', ?1, '%')) " +
             " or upper(i.description) like upper(concat('%', ?1, '%')))")
